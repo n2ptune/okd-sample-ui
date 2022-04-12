70c827400fc20e8ab5569234bdf65c55d4e0efc1
n2ptune <n2ptune@outlook.com>
2022-04-12 20:25:16 +0900

    에라이

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-axios.defaults.baseURL = process.env.NODE_ENV === 'production' ? process.env.APP_API_URL : 'http://localhost:3033'
+axios.defaults.baseURL = 'http://okd-sample-server-khlee.apps.lab.okd.local'
 
 export const write = (context: any) => {
   return axios.post('/note', context)
--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,16 @@
-import sveltePreprocess from 'svelte-preprocess'
+import sveltePreprocess from "svelte-preprocess"
+import * as dotenv from "dotenv"
+
+dotenv.config({ path: "./" })
 
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess()
+  preprocess: sveltePreprocess({
+    replace: [
+      ["process.env.NODE_ENV", JSON.stringify(process.env.NODE_ENV)],
+    ]
+  }),
+
+  vite: false
 }
